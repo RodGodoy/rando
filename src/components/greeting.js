01dbbe3c8565e92e8d0ffa53e9881c8f0555678a
RigoTheAmigo <rodgodoy93@gmail.com>
2021-02-09 18:28:49 -0600

    Components. Whupee.

---
+++ src/components/greeting.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Greeting = () => {
+    return(
+        <>
+        <h1>this is a component. would you look at that?</h1>
+        <h2>Well Shi-</h2>
+        <h3>smaller text</h3>
+        </>
+    )
+}
+
+export default Greeting
